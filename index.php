5c63ddbfe01538cbc4a02432ed472a185901430e
Tanvir <tanvir8269@gmail.com>
2017-05-17 00:42:30 +0800

    added search bar in the main menu in html section and also the styles for it

--- index.php
+++ index.php
@@ -19,6 +19,13 @@
 				<li><a href="#">Shopping Cart</a></li>
 				<li><a href="#">Contact Us</a></li>
 			</ul>
+			
+			<div id="form">
+				<form method="get" action="results.php" enctype="multipart/form-data">
+					<input type="text" name="user_query" placeholder="Search Products"/>
+					<input type="submit" name="search" value="Search"/>
+				</form>
+			</div>
 		</div>
 		<div class="content-wrapper">
 		
