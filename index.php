a7d83ee03ff29ff8f023b0b4b1eb7a489388c27d
My Laptop <tanvir8269@gmail.com>
2017-05-16 22:59:16 +0800

    updated styles.css(added css reset code) and added html sections in index.html file

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
 </head>
 <body>
 	<div class="main_wrapper">
-	hello world
+		<div class="header_wrapper">
+			<img id="logo" src="#"/>
+			<img id="banner" src="#"/>	
+		</div>
+		<div class="menubar">This is menu</div>
+		<div class="content-wrapper">
+		
+			<div id="sidebar">This is sidebar</div>
+		
+			<div id="content_area">This is content</div>
+		</div>
+		<div id="footer">This is footer</div>
 	</div>
 </body>
