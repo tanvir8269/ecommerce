4a377f90576a226cd48ded6a70c0cc5282e48afc
Tanvir <tanvir8269@gmail.com>
2017-05-16 21:45:39 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title>My Online Shop</title>
+	<link rel="stylesheet" href="styles/styles.css" media="all"/>
+</head>
+<body>
+	<div class="main_wrapper">
+	hello world
+	</div>
+</body>
